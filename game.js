8d147d16ebda931a69081d980a370e2ff331f83c
Majd Khasib <majd.khasib98@gmail.com>
2019-12-31 22:33:13 +0200

    Few modifications left

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ var gamePattern = [];
 var userClickedPattern = [];
 var level = 0;
 var toggle=true;
+var time=1;
 $(document).keypress(function(event){
   if(toggle)
   {
@@ -12,14 +13,54 @@ $(document).keypress(function(event){
 });
 $('.btn').click(function() {
   // console.log('You clicked button with ID:' + this.id);
-  var userChosenColour = this.id;
+  if(!toggle)
+{  var userChosenColour = this.id;
   userClickedPattern.push(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
   // console.log(userClickedPattern);
   playSound(userChosenColour);
   animatePress(userChosenColour);
-
+}
 
 });
+function checkAnswer(currentLevel){
+if(userClickedPattern[currentLevel]===gamePattern[currentLevel])
+{
+if(userClickedPattern.length==gamePattern.length)
+{
+  startSequence();
+  setTimeout(function(){nextSequence();},(time++)*2000);
+userClickedPattern=[];
+}
+else
+{
+
+}
+}
+else
+{
+playSound("wrong");
+$("body").addClass("game-over");
+$("#level-title").text("Game Over, Press Any Key to Restart");
+
+setTimeout(function() {
+  $("body").removeClass("game-over");;
+}, 200);
+gamePattern=[];
+userClickedPattern=[];
+level=0;
+toggle=true;
+}
+}
+function startSequence(){
+for(let i=0;i<gamePattern.length;i++)
+{
+  setTimeout(function timer(){  var clickedButton = $("#" + gamePattern[i]);
+    clickedButton.fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(gamePattern[i]);},2000);
+
+}
+}
 function nextSequence() {
   var randomNumber = Math.floor((Math.random() * 4));
   level += 1;
